889fcfc033a5b898fe69d75907af8a554f7bc2ed
Bruno Cardoso Cantisano <bruno.cantisano>
2026-06-05 15:05:43 -0300

    Utilizando platformio.ini

---
+++ include/WebServerHandler.h
@@ -0,0 +1,118 @@
+// WebServerHandler.h
+#ifndef WEBSERVERHANDLER_H
+#define WEBSERVERHANDLER_H
+
+#undef HTTP_GET
+#undef HTTP_POST
+#undef HTTP_PUT
+#undef HTTP_DELETE
+
+#if defined(ESP8266)
+/*******************************
+ *  Build para ESP8266
+ *******************************/
+	#include <ESP8266WiFi.h>
+#else // ESP32
+/*******************************
+ *  Build para ESP32
+ *******************************/
+	#include <WiFi.h>
+#endif
+
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <DNSServer.h>
+#include <pgmspace.h>   // PROGMEM
+#include <ArduinoJson.h>
+#include "Tipos.h" 
+#include "StorageHandler.h"
+#include "UtilsHandler.h"
+#include "PreferencesHandler.h"
+#include "HttpStatusCodes.h"
+#include <ArduinoUtilsCds.h>
+#include "WebMessages.h"
+#include <Regexp.h>
+#include "Config.h"
+
+#define MAX_PAYLOAD_SIZE           2000
+
+#define RelayWater                 D8
+#define RelayLight                 D7
+#define RelayLevel                 D6
+
+class WebServerHandler {
+private:
+    AsyncWebServer * server;
+    AsyncWebSocket * ws;      // rota do websocket
+    ArduinoUtilsCds * utilscds;
+    UtilsHandler * utilshdl;
+    StorageHandler * strhdl;
+    PreferencesHandler * prefshdl;
+    // Lista de sensores
+    ListaEncadeada<ArduinoSensorPort*> sensorListaEncadeada = ListaEncadeada<ArduinoSensorPort*>();
+    // Lista de aplicacoes do jenkins
+    ListaEncadeada<Agenda*> agendaListaEncadeada = ListaEncadeada<Agenda*>();
+
+    DNSServer dns;
+    String apiToken;
+    String apiVersion;
+    String host;
+    String callerOrigin;
+    String savedSsid;
+    String savedPass;
+    String obtemEstadoSensor(int sensor);
+    String obtemMetricas();
+    void atribuiMetrica(String *p, String metric, String value);
+    int obtemContagemBoots();
+    void incrementaContagemBoots();
+    bool check_authorization_header(AsyncWebServerRequest * request);
+    char payloadBuffer[MAX_PAYLOAD_SIZE];
+    void handleFileServing();
+    void handleHome();
+    void handleSwagger();
+    void handleSwaggerUI();
+    void handleHealth();
+    void handleMetrics();
+    void handlePorts();
+    void handleSensors();
+    void handleUpdateSensors();
+    void handleEventos();
+    void handleLists();
+    void handleInsertItemList();
+    void handleDeleteItemList();
+    void handleOptions();
+    void handleOnError();    
+    void registerPortalRoutes();
+    void notifySensors(const String& id, bool s25, bool s50, bool s75, bool s100);
+    void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len);
+public:
+    WebServerHandler(
+        const String& token, 
+        const String& version, 
+        const String& hostServer, 
+        const String& caller,
+        ArduinoUtilsCds * cds);
+    ~WebServerHandler();
+
+    void startWebServer(void);
+    void loop();
+    bool connectSTA(const String& hostForMDNS);
+    void startWebServerWifiManager(const String& apName);
+    bool loadSensorList();
+    bool addSensor(int id, int gpio, int status);
+    ArduinoSensorPort * searchListSensor(int gpio);
+    
+    int searchList(String dataAgenda);
+    bool validaHora(String hora);
+    void addAgenda(String dataAgenda);
+    void removeAgenda(int index);
+    void saveAgendaList();
+    int loadAgendaList();
+    void nivelBaixo();
+    void nivelAlto();
+    void ligarBomba();
+    void desligarBomba();
+    AsyncWebServer * getWebServer();
+};
+
+#endif
